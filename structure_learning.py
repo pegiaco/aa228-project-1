f448b67ce41753dfcf0c1508888f358eb73455fb
pegiaco <pegiacomelli@gmail.com>
2023-10-19 19:31:25 +0200

    Initial commit

---
+++ structure_learning.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import networkx as nx
+
+# Load data
+small_dataset = pd.read_csv("./data/small.csv")
+medium_dataset = pd.read_csv("./data/medium.csv")
+large_dataset = pd.read_csv("./data/large.csv")
+
+
